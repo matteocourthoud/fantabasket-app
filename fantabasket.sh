e139e6a084d89cb4f7b968f23a3b7be379a20b06
matteocourthoud <matteo.courthoud@uzh.ch>
2023-11-23 09:46:20 +0100

    Kill processes using port 8080 before serving dashboard

--- fantabasket.sh
+++ fantabasket.sh
@@ -5,6 +5,9 @@ cd $ABSPATH/src/
 # Prepare data for dashboard
 python3 main.py
 
+# Kill processes using port 8080
+lsof -t -i tcp:8080 | xargs kill
+
 # Serve dashboard
 open http://localhost:8080
 python3 dashboard/dashboard.py
