658090430e88e76bf6e1b9ea415030ffd9321e18
matteocourthoud <matteo.courthoud@uzh.ch>
2024-11-04 17:23:39 +0100

    Scrape ratings

---
+++ src/scraping/scrape_ratings.py
@@ -0,0 +1,97 @@
+"""
+Scrape NBA games stats.
+Author: Matteo Courthoud
+Date: 22/10/2022
+"""
+import os
+from datetime import datetime
+
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+SEASON = 2024
+INITIAL_RATINGS_FILE = "%i/ratings.csv"
+RATINGS_FILE = "%i/ratings.csv"
+WEBSITE_URL = 'https://basketballmonster.com/nbalineups.aspx'
+
+
+def extract_dunkest_table(driver):
+    # Locate the table rows
+    rows = driver.find_elements(By.CSS_SELECTOR, "table tbody tr")
+    data = []
+    for row in rows:
+        cols = row.find_elements(By.TAG_NAME, "td")
+        data.append([col.text for col in cols])
+        df = pd.DataFrame(data).iloc[:, [0, 4, 5]]
+    df.columns = ["name", "dunkest_value", "plus"]
+    return df
+
+
+def scrape_ratings(url: str):
+    # Initialize chrome driver
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Extract player data
+    driver.get(url)
+    driver.implicitly_wait(1)
+    df_current_ratings = pd.DataFrame()
+    while True:
+        df_current_ratings = pd.concat([df_current_ratings, extract_dunkest_table(driver)])
+        driver.implicitly_wait(0.1)
+        try:
+            link = driver.find_element(By.LINK_TEXT, '»')
+        except:
+            break
+        link.click()
+    driver.quit()
+    return df_current_ratings
+
+
+def update_ratings(data_dir: str, season: int):
+    file_path = os.path.join(data_dir, RATINGS_FILE % season)
+    date_today = datetime.now().date()
+
+    if os.path.exists(file_path):
+        df_ratings = pd.read_csv(file_path)
+        if date_today in df_ratings.date.unique():
+            return df_ratings
+    else:
+        df_ratings = pd.DataFrame()
+
+    print("Scraping ratings...")
+    url = f"https://www.dunkest.com/en/nba/stats/players/table?season_id={season - 2005}&mode=dunkest"
+    df_current_ratings = scrape_ratings(url=url)[["name", "rating"]]
+    df_current_ratings["date"] = date_today
+    df_ratings = pd.concat([df_ratings, df_current_ratings])
+    df_ratings.to_csv(file_path, index=False)
+    return df_ratings
+
+
+def scrape_initial_ratings():
+    # Extract player data
+    url = "https://www.dunkest.com/en/nba/stats/players/table?season_id=19&mode=dunkest&stats_type=tot"
+    url += "".join([f"&weeks[]={i}" for i in range(1, 6)])
+    url += "".join([f"&teams[]={i}" for i in range(1, 31)])
+    url += "&positions[]=1&positions[]=2&positions[]=3&player_search=&min_cr=4&max_cr=35&sort_by=pdk&sort_order=desc"
+    print("Verify that url works: \n", url)
+
+    df_ratings = scrape_ratings(url)
+    df_ratings["initial_rating"] = pd.to_numeric(df_ratings["dunkest_value"]) + pd.to_numeric(
+        df_ratings["plus"].str.replace("+", "").str.replace("−", "-"))
+    df_ratings = df_ratings[["name", "initial_rating"]]
+    return df_ratings
+
+
+def update_initial_ratings(data_dir: str, season: int) -> pd.DataFrame:
+    file_path = os.path.join(data_dir, INITIAL_RATINGS_FILE % season)
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path)
+    else:
+        print("Scraping initial ratings")
+        df_ratings = scrape_initial_ratings()
+        df_ratings.to_csv(file_path, index=False)
+        return df_ratings
