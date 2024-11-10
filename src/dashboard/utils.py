ea2de3067e8839b72ddf586605a0b16b9443a6cd
matteocourthoud <matteo.courthoud@uzh.ch>
2024-11-10 20:12:51 +0100

    Move functions from dashboard.py to utils.py

--- src/dashboard/utils.py
+++ src/dashboard/utils.py
@@ -1,56 +1,68 @@
-"""
-Author: Matteo Courthoud
-Date: 28/10/2022
-"""
+"""Dashboard Utilities."""
+import os
+from datetime import datetime
 
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pandas as pd
 
-# Remove warnings
-import warnings
-warnings.filterwarnings('ignore')
-
-# Plot settings
-plt.style.use('seaborn')
-sns.set_theme(style="ticks")
-mpl.rcParams['figure.figsize'] = (10,6)
-
-# Title
-mpl.rcParams['figure.titlesize'] = 22
-mpl.rcParams['figure.titleweight'] = 'bold'
-mpl.rcParams['axes.titlesize'] = 22
-mpl.rcParams['axes.titleweight'] = 'bold'
-mpl.rcParams['axes.titlepad'] = 20
-
-# Axes labels
-mpl.rcParams['axes.labelsize'] = 16
-mpl.rcParams['axes.labelweight'] = 'bold'
-
-# Grid and thicks
-mpl.rcParams['axes.spines.right'] = False
-mpl.rcParams['axes.spines.left'] = False
-mpl.rcParams['axes.spines.top'] = False
-mpl.rcParams['axes.spines.right'] = False
-mpl.rcParams['axes.grid'] = True
-mpl.rcParams['axes.grid.axis'] = 'y'
-#mpl.rcParams['axes.xmargin'] = 0
-mpl.rcParams['ytick.left'] = False
-
-# Legend
-mpl.rcParams['legend.facecolor'] = 'w'
-mpl.rcParams['legend.title_fontsize'] = 14
-mpl.rcParams['legend.fontsize'] = 12
-mpl.rcParams['legend.frameon'] = True
-mpl.rcParams['legend.framealpha'] = 1
-mpl.rcParams['legend.fancybox'] = True
-mpl.rcParams['legend.facecolor'] = 'white'
-mpl.rcParams['legend.edgecolor'] = 'gray'
-mpl.rcParams['legend.borderpad'] = 0.6
-
-# Other
-mpl.rcParams['lines.linewidth'] = 4
-mpl.rcParams['lines.markersize'] = 10
-mpl.rcParams['scatter.edgecolors'] = None
+PLAYERS_FILE = 'players.csv'
+GAMES_FILE = 'games.csv'
+FANTABASKET_STATS_FILE = 'fantabasket_stats.csv'
+PREDICTED_GAIN_FILE = 'predicted_gain.csv'
 
 
+def get_fantabasket_stats(data_dir: str, season: int) -> pd.DataFrame:
+    """Gets dataset with fantabasket statistics, with game dates."""
+    df_fanta_stats = pd.read_csv(os.path.join(data_dir, FANTABASKET_STATS_FILE))
+
+    # Add game dates to df_fanta_stats
+    games_path = os.path.join(data_dir, str(season), GAMES_FILE)
+    df_dates = pd.read_csv(games_path)[['date', 'game_id']].drop_duplicates()
+    df_dates['date'] = pd.to_datetime(df_dates['date'])
+    df_fanta_stats = pd.merge(df_fanta_stats, df_dates, on='game_id', how='right')
+
+    # Add time delta for filtering
+    df_fanta_stats['time_delta'] = (datetime.today() - df_fanta_stats.date).dt.days
+
+    # Add last price and gain
+    df_fanta_stats['last_price'] = df_fanta_stats.groupby('name')['fanta_value'].transform('last')
+
+    # Add predicted fantabasket gain
+    df_gain = pd.read_csv(os.path.join(data_dir, PREDICTED_GAIN_FILE))[['name', 'predicted_gain', 'status']]
+    df = pd.merge(df_fanta_stats, df_gain, on='name', how='left')
+    df['status'] = df['status'].fillna('')
+    return df_fanta_stats
+
+
+def get_players_last_stats(data_dir: str, season: int) -> pd.DataFrame:
+    """Gets player stats from the last game."""
+    df_fanta_stats = get_fantabasket_stats(data_dir=data_dir, season=season)
+
+    # Select the last game for each player
+    df_last_dates = df_fanta_stats.groupby("name", as_index=False)["date"].max()
+    df_last_stats = pd.merge(df_fanta_stats, df_last_dates, on=["name", "date"], how="inner")
+
+    # Add players positions to df_fanta_stats
+    players_path = os.path.join(data_dir, PLAYERS_FILE)
+    df_last_stats = pd.merge(df_last_stats, pd.read_csv(players_path), on='name', how='left')
+    return df_last_stats
+
+
+def get_df_timeseries_plot(data_dir: str, season: int) -> pd.DataFrame:
+    """Gets dataframe for time series plot in the dashboard."""
+    df_ts_plot = get_fantabasket_stats(data_dir=data_dir, season=season)
+    df_ts_plot['name'] = df_ts_plot['name'] + ' - ' + df_ts_plot['last_price'].apply(lambda x: '{0:.1f}'.format(x))
+    return df_ts_plot
+
+
+def get_df_stats_table(data_dir: str, season: int) -> pd.DataFrame:
+    """Gets dataframe for statistics table in the dashboard."""
+    df_stats_table = get_players_last_stats(data_dir=data_dir, season=season)
+    df_stats_table['last_date'] = df_stats_table.groupby('name')['date'].transform('max')
+    df_stats_table = df_stats_table[df_stats_table.date == df_stats_table.last_date]
+    df_stats_table = df_stats_table[df_stats_table.date >= (pd.Timestamp.now().normalize() - pd.Timedelta(3, 'd'))]
+    df_stats_table = df_stats_table[['name', 'position', 'fanta_value', 'fanta_score', 'fanta_gain',
+                       'mp', 'pts', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf']]
+    df_stats_table.columns = ['Name', 'Role', 'Value', 'Score', 'Gain',
+                       'min', 'pts', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf']
+    df_stats_table = df_stats_table.sort_values('Gain', ascending=False).reset_index(drop=True).round(1)
+    return df_stats_table
