9f3d5cea2c7b95165b4f8d5aa7555cf1373ba976
matteocourthoud <matteo.courthoud@uzh.ch>
2024-11-12 10:24:15 +0100

    Rename df_stats to df_fanta_stats

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
+"""Main file to prepare data for the fantabasket dashboard."""
+
+import os
+import pandas as pd
+
 from scraping.scrape_calendar import update_get_nba_calendar
 from scraping.scrape_injuries import update_get_nba_injuries
-from scraping.scrape_upcoming_lineups import update_nba_lineups
-from scraping.scrape_games import Scraper
-from model.compute_stats import FantabasketStats
+from scraping.scrape_next_lineups import update_get_next_lineups
+from scraping.scrape_games import update_get_nba_stats
+from model.compute_fanta_stats import update_get_fantabasket_stats
 from model.predict_gain import GainModel
 
 DATA_DIR = "../data"
 SEASON = 2024
 
 if __name__ == '__main__':
+    # Load game data
+    df_games = pd.read_csv(os.path.join(DATA_DIR, str(SEASON), "games.csv"))
+
     # Scrape calendar
     df_calendar = update_get_nba_calendar(data_dir=DATA_DIR, season=SEASON)
 
@@ -16,18 +24,15 @@ if __name__ == '__main__':
     df_injuries = update_get_nba_injuries(data_dir=DATA_DIR, update=True)
 
     # Scrape lineups
-    df_lineups = update_nba_lineups(data_dir=DATA_DIR, season=SEASON)
+    df_lineups = update_get_next_lineups(data_dir=DATA_DIR, season=SEASON)
 
     # Scrape stats
-    scraper = Scraper(data_dir=DATA_DIR, season=SEASON, df_calendar=df_calendar)
-    df_stats = scraper.update_get_nba_stats()
+    df_stats = update_get_nba_stats(data_dir=DATA_DIR, season=SEASON)
 
     # Compute fantabasket stats
-    ds = FantabasketStats(data_dir=DATA_DIR, season=SEASON, df_stats=df_stats)
-    df_stats = ds.update_get_fantabasket_stats()
-    df_games = ds.load_df("games.csv")
+    df_fanta_stats = update_get_fantabasket_stats(data_dir=DATA_DIR, season=SEASON, df_stats=df_stats)
 
     # Compute predicted gain
     gm = GainModel(data_dir=DATA_DIR, season=SEASON, df_calendar=df_calendar, df_injuries=df_injuries,
-                   df_games=df_games, df_stats=df_stats)
+                   df_games=df_games, df_fanta_stats=df_fanta_stats)
     df_predicted_gain = gm.update_get_predicted_gain()
